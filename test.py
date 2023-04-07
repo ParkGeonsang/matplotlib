8c2ef17290dbed826897962e9fdd120ce9a09faa
keisa <skycindy89@naver.com>
2023-04-07 18:19:27 +0900

    Initial commit

---
+++ test.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+
+# x = ['combing', 'drinking', 'feeding', 'napping', 'palying', 'washing']
+#
+# plt.plot(x, ['happy', 'happy', 'happy', 'happy', 'board', 'board'], label='dog', color='blue'),
+# plt.plot(x, ['board', 'happy', 'board', 'board', 'happy', 'happy',], label='cat', color='red'),
+#
+# plt.legend()
+# plt.show()
+
+# num = [10, 15, 5, 20]
+# fr = ['apple', 'orange', 'lemon', 'lime']
+#
+# x, y = plt.subplots(1, 3, sharey=True, sharex=True)
+# # plt.subplot(1, 3, 1)
+# y[0].bar(fr, num)
+# # plt.subplot(1, 3, 2)
+# y[1].scatter(fr, num)
+# # plt.subplot(1, 3, 3)
+# y[2].plot(fr, num)
+# plt.show()
+
+# x = [1, 2, 3, 4]
+# y = [1, 2, 3, 4]
+#
+# plt.subplot(1, 2, 1)
+# plt.plot(x,y)
+#
+#
+# plt.subplot(1, 2, 2)
+# plt.plot(x,y)
+# plt.show()
+# plt.plot([10,20,30,40]) #선그래프
+# plt.show()
+
+# plt.plot([1, 2, 3, 4], [12, 53, 42, 13])
+# plt.show()
+
+# plt.title('plot')
+# plt.plot([10, 20, 30, 40], label='start' , color='pink', linestyle='--'),
+# plt.plot([40, 30, 20, 10], label='end', color='red', ls='--'),
+# plt.legend()
+# plt.show()
+
+# plt.title('plot')
+# plt.scatter([10, 20, 30, 40], [20, 30, 40, 50])
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.show()
+
+# import csv
+# f = open('weather.csv')
+# data=csv.reader(f)
+# next(data)
+#
+# for i in data:
+#     print(i[1])
